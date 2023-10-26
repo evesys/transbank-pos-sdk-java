a74d9e04d122db95ee451ed333a8a7f7c81f128e
pedro <lanixerp@lanixerp-VirtualBox>
2023-10-26 12:33:49 -0300

    Formatted the code.

--- src/main/java/cl/transbank/pos/POSIntegrado.java
+++ src/main/java/cl/transbank/pos/POSIntegrado.java
@@ -1,5 +1,6 @@
 package cl.transbank.pos;
 
+import cl.transbank.pos.exceptions.common.TransbankConnectionException;
 import cl.transbank.pos.exceptions.common.TransbankException;
 import cl.transbank.pos.exceptions.common.TransbankLoadKeysException;
 import cl.transbank.pos.exceptions.common.TransbankRefundException;
@@ -17,21 +18,17 @@ import java.util.List;
 @Log4j2
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 public class POSIntegrado extends Serial {
-    public boolean poll() throws TransbankException {
-        try {
-            checkCanWrite();
-            String command = createCommand("0100");
-            byte[] hexCommand = command.getBytes();
 
-            log.debug(String.format("Request [Hex]: %s", toHexString(hexCommand)));
-            log.debug(String.format("Request [ASCII]: %s", command));
+    public boolean poll() throws TransbankConnectionException {
+        checkCanWrite();
+        String command = createCommand("0100");
+        byte[] hexCommand = command.getBytes();
 
-            port.writeBytes(hexCommand, hexCommand.length);
-            return checkAck();
+        log.debug(String.format("Request [Hex]: %s", toHexString(hexCommand)));
+        log.debug(String.format("Request [ASCII]: %s", command));
 
-        } catch (TransbankException e) {
-            throw new TransbankException("Unable to send Poll command on port", e);
-        }
+        port.writeBytes(hexCommand, hexCommand.length);
+        return checkAck();
     }
 
     public boolean setNormalMode() throws TransbankException {
@@ -62,9 +59,15 @@ public class POSIntegrado extends Serial {
     }
 
     public SaleResponse sale(int amount, String ticket, boolean sendStatus) throws TransbankSaleException {
-        if (amount < 50) throw new TransbankSaleException("Amount must be greater than 50.");
-        if (amount > 999999999) throw new TransbankSaleException("Amount must be less than 999999999.");
-        if (ticket.trim().length() > 6) throw new TransbankSaleException("Ticket must be up to 6 in length");
+        if (amount < 50) {
+            throw new TransbankSaleException("Amount must be greater than 50.");
+        }
+        if (amount > 999999999) {
+            throw new TransbankSaleException("Amount must be less than 999999999.");
+        }
+        if (ticket.trim().length() > 6) {
+            throw new TransbankSaleException("Ticket must be up to 6 in length");
+        }
 
         String command = String.format("0200|%s|%s|||%s|", amount, ticket, sendStatus ? 1 : 0);
 
@@ -79,9 +82,15 @@ public class POSIntegrado extends Serial {
     }
 
     public MultiCodeSaleResponse multiCodeSale(int amount, String ticket, long commerceCode, boolean sendStatus) throws TransbankMultiCodeSaleException {
-        if (amount < 50) throw new TransbankMultiCodeSaleException("Amount must be greater than 50.");
-        if (amount > 999999999) throw new TransbankMultiCodeSaleException("Amount must be less than 999999999.");
-        if (ticket.trim().length() > 6) throw new TransbankMultiCodeSaleException("Ticket must be up to 6 in length");
+        if (amount < 50) {
+            throw new TransbankMultiCodeSaleException("Amount must be greater than 50.");
+        }
+        if (amount > 999999999) {
+            throw new TransbankMultiCodeSaleException("Amount must be less than 999999999.");
+        }
+        if (ticket.trim().length() > 6) {
+            throw new TransbankMultiCodeSaleException("Ticket must be up to 6 in length");
+        }
 
         String command = String.format("0270|%s|%s|||%s|%s|", amount, ticket, sendStatus ? 1 : 0, commerceCode);
 
